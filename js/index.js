d1453ad9e973a01daee103d511e33b18678408ef
José Carlos <jcddsj01@outlook.com>
2022-11-25 14:50:25 -0300

    Alteraçõs reponsividade

--- js/index.js
+++ js/index.js
@@ -9,24 +9,24 @@ function CalcularIMC() {
     let imc = peso / (altura ** 2)
 
     if (imc < 18.5) {
-        document.getElementById("imc-magreza").style.backgroundColor = '#cfcfcf';
+        document.getElementById("imc-magreza").style.backgroundColor = '#ff52028c';
     } else if (imc < 25) {
-        document.getElementById("imc-normal").style.backgroundColor = '#cfcfcf';
+        document.getElementById("imc-normal").style.backgroundColor = '#ff52028c';
     } else if (imc < 30) {
-        document.getElementById("imc-sobrepeso").style.backgroundColor = '#cfcfcf';
+        document.getElementById("imc-sobrepeso").style.backgroundColor = '#ff52028c';
     } else if (imc < 40) {
-        document.getElementById("imc-obesidade").style.backgroundColor = '#cfcfcf';
+        document.getElementById("imc-obesidade").style.backgroundColor = '#ff52028c';
     } else {
-        document.getElementById("imc-obesidade-grave").style.backgroundColor = '#cfcfcf';
+        document.getElementById("imc-obesidade-grave").style.backgroundColor = '#ff52028c';
     }
 
-    document.getElementById("resultado").innerText = 'SEU IMC: ' + imc.toFixed(2).replace(".", ",");
+    document.getElementById("resultado").innerText = imc.toFixed(2).replace(".", ",");
 }
 
 function Limpar() {
     document.getElementById("altura").value = "";
     document.getElementById("peso").value = "";
-    document.getElementById("resultado").innerHTML = "SEU IMC: ";
+    document.getElementById("resultado").innerHTML = "SEU IMC.: ";
     document.getElementById("imc-magreza").style.backgroundColor = '#fff';
     document.getElementById("imc-normal").style.backgroundColor = '#fff';
     document.getElementById("imc-sobrepeso").style.backgroundColor = '#fff';

0735db2d8390fb7158968d1e1a04b65fb323b43a
José Carlos <jcddsj01@outlook.com>
2022-11-27 14:30:59 -0300

    Alterações

--- js/index.js
+++ js/index.js
@@ -7,17 +7,22 @@ function CalcularIMC() {
     let altura = document.getElementById("altura").value.replace(',', '.');
     let peso = document.getElementById("peso").value.replace(',', '.');
     let imc = peso / (altura ** 2)
-
+    
     if (imc < 18.5) {
-        document.getElementById("imc-magreza").style.backgroundColor = '#ff52028c';
+        document.getElementById("imc-magreza").style.color = '#ff5202';
+        document.getElementById("imc-magreza").style.fontWeight = 'bolder';
     } else if (imc < 25) {
-        document.getElementById("imc-normal").style.backgroundColor = '#ff52028c';
+        document.getElementById("imc-normal").style.color = '#ff5202';
+        document.getElementById("imc-normal").style.fontWeight = 'bolder';
     } else if (imc < 30) {
-        document.getElementById("imc-sobrepeso").style.backgroundColor = '#ff52028c';
+        document.getElementById("imc-sobrepeso").style.color = '#ff5202';
+        document.getElementById("imc-sobrepeso").style.fontWeight = 'bolder';
     } else if (imc < 40) {
-        document.getElementById("imc-obesidade").style.backgroundColor = '#ff52028c';
+        document.getElementById("imc-obesidade").style.color = '#ff5202';
+        document.getElementById("imc-obesidade").style.fontWeight = 'bolder';
     } else {
-        document.getElementById("imc-obesidade-grave").style.backgroundColor = '#ff52028c';
+        document.getElementById("imc-obesidade-grave").style.color = '#ff5202';
+        document.getElementById("imc-obesidade-grave").style.fontWeight = 'bolder';
     }
 
     document.getElementById("resultado").innerText = imc.toFixed(2).replace(".", ",");
@@ -27,9 +32,14 @@ function Limpar() {
     document.getElementById("altura").value = "";
     document.getElementById("peso").value = "";
     document.getElementById("resultado").innerHTML = "SEU IMC.: ";
-    document.getElementById("imc-magreza").style.backgroundColor = '#fff';
-    document.getElementById("imc-normal").style.backgroundColor = '#fff';
-    document.getElementById("imc-sobrepeso").style.backgroundColor = '#fff';
-    document.getElementById("imc-obesidade").style.backgroundColor = '#fff';
-    document.getElementById("imc-obesidade-grave").style.backgroundColor = '#fff';
-} 
+    document.getElementById("imc-magreza").style.color = '#000';
+    document.getElementById("imc-normal").style.color = '#000';
+    document.getElementById("imc-sobrepeso").style.color = '#000';
+    document.getElementById("imc-obesidade").style.color = '#000';
+    document.getElementById("imc-obesidade-grave").style.color = '#000';
+    document.getElementById("imc-magreza").style.fontWeight = 'normal';
+    document.getElementById("imc-normal").style.fontWeight = 'normal';
+    document.getElementById("imc-sobrepeso").style.fontWeight = 'normal';
+    document.getElementById("imc-obesidade").style.fontWeight = 'normal';
+    document.getElementById("imc-obesidade-grave").style.fontWeight = 'normal';
+}

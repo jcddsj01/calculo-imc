80d3e9b18e87d701616d671b3e8d062e38ebccae
José Carlos <116307228+jcddsj01@users.noreply.github.com>
2023-08-07 19:05:53 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,63 @@
+$(document).ready(function() {
+    initializeMasks();
+});
+
+function initializeMasks() {
+    $("#altura").mask("#,##");
+
+    const pesoInput = $("#peso");
+    pesoInput.mask(pesoInput.val().length > 6 ? "###,##" : "##,##");
+}
+
+function changeElementStyle(id, fontWeight, backgroundColor) {
+    const element = document.getElementById(id);
+    element.style.fontWeight = fontWeight;
+    element.style.backgroundColor = backgroundColor;
+}
+
+function CalcularIMC() {
+    const altura = parseFloat(document.getElementById("altura").value.replace(',', '.'));
+    const peso = parseFloat(document.getElementById("peso").value.replace(',', '.'));
+    const imc = peso / (altura ** 2);
+
+    const ranges = [
+        { id: "abaixo-do-peso", min: 0, max: 18.4 },
+        { id: "peso-normal", min: 18.5, max: 24.9 },
+        { id: "sobrepeso", min: 25, max: 29.9 },
+        { id: "obesidade-grau-um", min: 30, max: 34.9 },
+        { id: "obesidade-grau-dois", min: 35, max: 39.9 },
+        { id: "obesidade-grau-tres-ou-morbida", min: 40, max: Infinity }
+    ];
+
+    for (const range of ranges) {
+        if (imc >= range.min && imc <= range.max) {
+            changeElementStyle(range.id, 'bolder', '#e7e7e7');
+        } else {
+            changeElementStyle(range.id, 'normal', '#fff');
+        }
+    }
+
+    const resultadoElement = document.getElementById("resultado");
+    resultadoElement.innerText = `SEU IMC: ${imc.toFixed(2).replace(".", ",")}`;
+    resultadoElement.style.fontWeight = 'bolder';
+    resultadoElement.style.fontSize = '1em'; /* 16px */
+}
+
+function Limpar() {
+    const ids = [
+        "altura", "peso", "resultado",
+        "abaixo-do-peso", "peso-normal", "sobrepeso",
+        "obesidade-grau-um", "obesidade-grau-dois", "obesidade-grau-tres-ou-morbida"
+    ];
+
+    for (const id of ids) {
+        const element = document.getElementById(id);
+        element.value = "";
+
+        if (id === "resultado") {
+            element.innerHTML = "SEU IMC: ";
+        } else {
+            changeElementStyle(id, 'normal', '#fff');
+        }
+    }
+}
